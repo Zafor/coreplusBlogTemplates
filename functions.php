f84dd8949b1d62ec040003af3015599193f7229a
Zafor Iqbal <zaforiqbal13@gmail.com>
2021-08-24 07:37:22 +0600

    coreplus subscription done

---
+++ functions.php
@@ -0,0 +1,130 @@
+<?php
+
+require_once 'app/helpers.php';
+require_once 'vendor/autoload.php';
+
+if (!defined('THEME_NAME')) {
+    define('THEME_NAME', 'TRUE');
+}
+
+/**
+ * Load environment
+ */
+$dotenv = new Dotenv\Dotenv(__DIR__);
+$dotenv->load();
+
+/**
+ * Roots includes
+ */
+require_once locate_template('/app/roots/utils.php');           // Utility functions
+require_once locate_template('/app/roots/init.php');            // Initial theme setup and constants
+require_once locate_template('/app/roots/wrapper.php');         // Theme wrapper class
+require_once locate_template('/app/roots/sidebar.php');         // Sidebar class
+require_once locate_template('/app/roots/config.php');          // Configuration
+require_once locate_template('/app/roots/titles.php');          // Page titles
+require_once locate_template('/app/roots/cleanup.php');         // Cleanup
+require_once locate_template('/app/roots/nav.php');             // Custom nav modifications
+require_once locate_template('/app/roots/gallery.php');         // Custom [gallery] modifications
+require_once locate_template('/app/roots/comments.php');        // Custom comments modifications
+require_once locate_template('/app/roots/relative-urls.php');   // Root relative URLs
+require_once locate_template('/app/roots/widgets.php');         // Sidebars and widgets
+require_once locate_template('/app/roots/scripts.php');         // Scripts and stylesheets
+require_once locate_template('/app/roots/custom.php');          // Custom functions
+
+require_once 'app/core.php';
+
+function coreplus_custom_excerpt_length($length)
+{
+    return 15;
+}
+add_filter('excerpt_length', 'coreplus_custom_excerpt_length', 999);
+
+function prefix_wcount()
+{
+    ob_start();
+    the_content();
+    $content = ob_get_clean();
+    return sizeof(explode(" ", $content));
+}
+
+function change_wp_search_size($query)
+{
+    if ($query->is_search) // Make sure it is a search page
+        $query->query_vars['posts_per_page'] = -1; // number of posts you would like to show
+
+    return $query; // Return our modified query variables
+}
+add_filter('pre_get_posts', 'change_wp_search_size'); // Hook our custom function onto the request filter
+
+// add_filter( 'the_title', 'coreplus_blog_title_shortner' );
+
+// function coreplus_blog_title_shortner( $title )
+// {
+//     // limit to 6 words
+//     return wp_trim_words( $title, 6, '...' );
+// }
+// 
+
+function limit_my_title($text, $limit)
+{
+    if (str_word_count($text, 0) > $limit) {
+        $words = str_word_count($text, 2);
+        $pos   = array_keys($words);
+        $text  = substr($text, 0, $pos[$limit]) . '...';
+    }
+    return $text;
+}
+
+function add_svg_to_upload_mimes($upload_mimes)
+{
+    $upload_mimes['svg'] = 'image/svg+xml';
+    $upload_mimes['svgz'] = 'image/svg+xml';
+    return $upload_mimes;
+}
+add_filter('upload_mimes', 'add_svg_to_upload_mimes', 10, 1);
+
+function register_acf_windsor()
+{
+    \Windsor\Capsule\Manager::make()->register();
+}
+add_action('acf/init', 'register_acf_windsor');
+
+function remove_block_css()
+{
+    wp_dequeue_style('wp-block-library');
+}
+add_action('wp_enqueue_scripts', 'remove_block_css', 100);
+
+
+// For ajax form submission ; coreplus blog subscribers
+
+add_action('wp_enqueue_scripts', 'enqueue_jquery_form');
+
+function enqueue_jquery_form()
+{
+    wp_enqueue_script('jquery-form');
+}
+
+add_action('wp_ajax_create_subscriber', 'create_subscriber');
+add_action("wp_ajax_nopriv_create_subscriber", "create_subscriber");
+function create_subscriber()
+{
+    $email = $_POST['blog-subscriber-email'];
+
+    $to = 'zaforiqbal13@gmail.com';
+    $subject = 'coreplus new subscriber';
+    $body = $email;
+    $headers = array('Content-Type: text/html; charset=UTF-8');
+    wp_mail($to, $subject, $body, $headers);
+
+    // assumes $to, $subject, $message have already been defined earlier...
+
+    // $headers[] = 'From: Me Myself <me@example.net>';
+    // $headers[] = 'Cc: John Q Codex <jqc@wordpress.org>';
+    // $headers[] = 'Cc: iluvwp@wordpress.org'; // note you can just use a simple email address
+
+    // wp_mail($to, $subject, $message, $headers);
+
+    wp_send_json_success($email);
+    wp_die();
+}
--- partials/subscribe-for-blogs.php
+++ partials/subscribe-for-blogs.php
@@ -4,9 +4,33 @@
             <img src="https://qa-web.coreplus.com.au/wp-content/uploads/2021/06/cta-img2.png" alt="">
         </div>
         <div class="cta-button col-md-6 col-md-pull-6 margin-top-40">
-            <p>Don’t Want to Miss Coreplus Stories & Updates!</p>
-            <input type="text" class="btn blog-subscriber-email no-wrap" placeholder="Enter your Email Address">
-            <a href="" class="btn section-button-white">&nbsp;&nbsp;&nbsp;&nbsp;Subscribe&nbsp;&nbsp;&nbsp;&nbsp;</a>
+            <p id="successText">Don’t Want to Miss Coreplus Stories & Updates!</p>
+            <form id="subscription-form" method="POST" name="subscriber-email" action="<?php echo admin_url('admin-ajax.php'); ?>">
+                <input type="text" name="blog-subscriber-email" id="subscriptionInput" class="btn blog-subscriber-email no-wrap" required placeholder="Enter your Email Address">
+                <input type="hidden" name="action" value="create_subscriber">
+                <input type="submit" id="subscriptionButton" value="Subscribe" class="btn section-button-white">
+            </form>
+            <script type="text/javascript">
+                jQuery(document).ready(function($) {
+                    $('#subscription-form').ajaxForm({
+                        success: function(response) {
+                            // console.log(response);
+                            document.getElementById("successText").innerHTML = "Thanks! You're subscribed to coreplus stories & updates";
+                            document.getElementById("subscriptionInput").placeholder = response.data;
+                            document.getElementById("subscriptionInput").style.border = '1px solid #61c5ba';
+                            document.getElementById("subscriptionButton").style.border = '1px solid #61c5ba';
+                            document.getElementById("subscriptionButton").type = "image";
+                            document.getElementById("subscriptionButton").src = "https://qa-web.coreplus.com.au/wp-content/uploads/2021/08/check-circle-regular.png";
+                            document.getElementById("subscriptionButton").style.height = '41px';
+                            document.getElementById("subscriptionButton").style.width = 'auto';
+
+                        },
+                        resetForm: true
+                    });
+                });
+            </script>
         </div>
+
+
     </div>
 </div>
